4cf53018220aed1c4ad206e7ef5a3cef6053df6c
JNunez06 <jecner.nunez@gmail.com>
2025-01-31 20:30:57 -0500

    se quito B.E y se puso tailwind

--- src/Components/Team.tsx
+++ src/Components/Team.tsx
@@ -1,37 +1,40 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import {default as JsonData} from "../data/data.json";
+import { default as JsonData } from "../data/data.json";
 
-
-export function Team()
-{
-    return(
-    
-    <div id='Team' className="h-screen bg-red-100">
-      <Container>
-        <Col md={{span:8, offset:2}} className=' section-title'>
-          <h2>CONOCE AL EQUIPO</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+export function Team() {
+  return (
+    <div id="Team" className="h-screen bg-red-100 dark:bg-red-900 transition-colors duration-300 py-16">
+      <div className="max-w-6xl mx-auto px-4">
+        {/* Título y descripción */}
+        <div className="text-center mb-12">
+          <h2 className="text-4xl font-bold text-black dark:text-white mb-4">
+            CONOCE AL EQUIPO
+          </h2>
+          <p className="text-xl text-gray-700 dark:text-gray-300">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.
           </p>
-        </Col>
-        <Row>
+        </div>
+
+        {/* Lista de miembros del equipo */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
           {JsonData
             ? JsonData.Team.map((d, i) => (
-                <Col md={3} sm={6} key={`${d.name}-${i}`} className=' team'>
-                  <div className='thumbnail'>
-                    {' '}
-                    <img src={d.img} alt='...' className='team-img' />
-                    <div className='caption'>
-                      <h4>{d.name}</h4>
-                      <p>{d.job}</p>
+                <div key={`${d.name}-${i}`} className="team bg-white dark:bg-neutral-800 p-6 rounded-lg shadow-lg transition-transform duration-300 hover:scale-105">
+                  <div className="thumbnail">
+                    <img src={d.img} alt={d.name} className="w-full h-48 object-cover rounded-lg mb-4" />
+                    <div className="caption text-center">
+                      <h4 className="text-2xl font-semibold text-black dark:text-white mb-2">
+                        {d.name}
+                      </h4>
+                      <p className="text-gray-700 dark:text-gray-300">
+                        {d.job}
+                      </p>
                     </div>
                   </div>
-                </Col>
+                </div>
               ))
             : 'loading'}
-        </Row>
-      </Container>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
